c68f44679e81b54290107a0161c6284665355f7f
Danils0n23 <danilsonjuniorbengui23@gmail.com>
2024-02-13 20:18:30 +0100

    mongoDB AND PRISMA ON PROJECT

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Input from "@/app/components/Inputs/Input";
+import Button from "@/app/components/Button";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -57,27 +60,115 @@ const AuthForm = () => {
     "
     >
       <div
-         className="
+        className="
         bg-white
         px-4
         py-8
         shadow
         sm:rounded-lg
         sm:px-10
-        "   
-       >
-
-        <form 
-        className="space-y-6"
-        onSubmit={handleSubmit(onSubmit)}
-        >
-          <Input 
-          id="email" 
-          label= "Email"
-          register={register}
-          errors={errors}
+        "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
           />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <div>
+              <Button disabled={isLoading} fullWidth type="submit">
+                {variant === "LOGIN" ? "Sign in" : "Register"}
+              </Button>
+            </div>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute
+                inset-0
+                flex
+                items-center
+              "
+            >
+              <div
+                className="
+              w-full 
+              border-t 
+              border-gray-300"
+              />
+            </div>
+            <div
+              className="
+                relative
+                flex 
+                justify-center
+                text-sm"
+            >
+              <span
+                className="
+              bg-white px-2
+              text-gray-500"
+              >
+                or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div
+          className="
+        flex
+        gap-2
+        justify-center
+        text-sm
+        mt-6
+        px-2
+        text-gray-500
+        "
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "New to Messenger?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
